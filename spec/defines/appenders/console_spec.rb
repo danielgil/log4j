127608c86119f5fcca4a733eb2bd271bcfb333c0
Daniel Gil <daniel.gil.bayo@gmail.com>
2015-01-23 15:05:50 +0100

    Adds Console appender

--- spec/defines/appenders/console_spec.rb
+++ spec/defines/appenders/console_spec.rb
@@ -10,12 +10,24 @@ describe 'log4j::appenders::console' do
     } }
   let(:params) {{
       :path => '/tmp/test.xml',
+      :target => 'SYSTEM_OUT',
+      :ignoreexceptions => false,
   }}
 
   it { should compile.with_all_deps }
   it { should contain_log4j__appenders__console('test')}
 
-  it { should contain_augeas('appender-test') }
+  it { should contain_augeas('appender-test').with({
+       'incl'    => '/tmp/test.xml',
+       'lens'    => 'Xml.lns',
+       'changes' => [
+         "set Configuration/Appenders/Console[./#attribute/name = 'test']/#attribute/name 'test'",
+         "set Configuration/Appenders/Console[./#attribute/name = 'test']/#attribute/follow true",
+         "set Configuration/Appenders/Console[./#attribute/name = 'test']/#attribute/target SYSTEM_OUT",
+         "set Configuration/Appenders/Console[./#attribute/name = 'test']/#attribute/ignoreExceptions false",
+         "set Configuration/Appenders/Console[./#attribute/name = 'test']/PatternLayout/#attribute/pattern '%m%n'",
+       ]
+   })}
 
   it { should contain_augeas('appenderref-test').with({
         'incl'    => '/tmp/test.xml',
